e46e90dbfd2b6432bd4be5fb3612b24c34774cdb
lutiku <lutiku.ndongala@edu.devinci.fr>
2020-11-09 00:04:30 +0100

    Create index.js

---
+++ Script/index.js
@@ -0,0 +1,54 @@
+
+//3
+var firstName = 'Lutiku'
+
+//4
+var favouriteColor = 'Black'
+
+//5
+
+ var city = 'Lubumbashi'
+ var country = 'Congo'
+
+var favoriteFood = 'Burger'
+
+var city = 'Paris'
+
+//ceci est un commmentaire
+
+console.log(firstName);
+console.log(favouriteColor);
+console.log(favoriteFood);
+console.log(city)
+
+//10
+
+var phrase = "A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all."
+
+var endingPhrase = " is what we see. Rainbow colours, are made with light bending"
+
+var one  = 'Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+
+var joined = ' '+one +', '+two+', '+three+', '+four+', '+five+', '+six+', '+seven+' '
+
+console.log(phrase+joined+endingPhrase)
+
+//13
+
+function myAlphabetLength(x) {
+
+ console.log(x.length)
+ 
+}
+
+myAlphabet = ["A", "B", "C"];
+myAlphabetLength(myAlphabet);
+
+
+
